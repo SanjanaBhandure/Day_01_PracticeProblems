5c9711e342f818b2b2d03594e5558123a4870d48
unknown <sanjana@example.com>
2022-04-04 00:35:29 +0530

    To display any Message

---
+++ DisplayMessage.java
@@ -0,0 +1,8 @@
+//Write a Program to display any Message
+package Bridgelabz;
+
+public class DisplayMessage {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Welcome to Java");
+    }
+}
