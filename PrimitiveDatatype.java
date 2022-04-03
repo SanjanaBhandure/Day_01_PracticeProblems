734006882506ff40df60e31a3fe74b42e965c542
unknown <sanjana@example.com>
2022-04-04 00:59:45 +0530

    To display default values of primitive datatype

---
+++ PrimitiveDatatype.java
@@ -0,0 +1,25 @@
+// Write a Java program to display the default value of all primitive data types of Java
+package Bridgelabz;
+
+public class PrimitiveDatatype {
+    static byte val1;
+    static short val2;
+    static int val3;
+    static float val4;
+    static char val5;
+    static long val6;
+    static double val7;
+    static boolean val8;
+
+    public static void main(String[] args) {
+        System.out.println("The default values of all the primitive data types");
+        System.out.println("Default value of byte: " +val1);
+        System.out.println("Default value of short: " +val2);
+        System.out.println("Default value of int: " +val3);
+        System.out.println("Default value of float: " +val4);
+        System.out.println("Default value of char: " +val5);
+        System.out.println("Default value of long: " +val6);
+        System.out.println("Default value of double: " +val7);
+        System.out.println("Default value of boolean: " +val8);
+    }
+}
